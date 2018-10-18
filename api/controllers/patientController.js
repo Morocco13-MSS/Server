dfac6d4bc8ac13deca8b75ce8cec938f7d4ed678
Jinzi Huang <jinzi.huang@gmail.com>
2018-10-18 01:54:03 +0100

    response json formatted data

--- api/controllers/patientController.js
+++ api/controllers/patientController.js
@@ -5,5 +5,37 @@ exports.getPatientAge = function(req, res) {
         //For exampleL: req.params.ageRange will be used a paratmeter from UI
         //res will be the response result back to UI
 
-    res.send('Hi Guys...This is the place you need to retrevie database');
+    var asaResult = {
+        asa: {
+            gt2: 90,
+            others: 57,
+            missing: 10
+        },
+        data2: [
+            {x: 0, y: 8},
+            {x: 1, y: 5},
+            {x: 2, y: 4},
+            {x: 3, y: 9},
+            {x: 4, y: 1},
+            {x: 5, y: 7},
+            {x: 6, y: 6},
+            {x: 7, y: 3},
+            {x: 8, y: 2},
+            {x: 9, y: 0}
+        ],
+        data3: [
+            {x: 0, y: 8},
+            {x: 1, y: 5},
+            {x: 2, y: 4},
+            {x: 3, y: 9},
+            {x: 4, y: 1},
+            {x: 5, y: 7},
+            {x: 9, y: 6},
+            {x: 7, y: 3},
+            {x: 8, y: 2},
+            {x: 9, y: 0}
+          ]
+
+    }
+    res.send(asaResult); //json
 };
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var express = require('express'),
   port = process.env.PORT || 8080,
 
   bodyParser = require('body-parser');
+var cors = require('cors');
 
 // TODO: Muruli, the mysql database should be connected here
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 var routes = require('./api/routes/patientRoutes'); //importing route
