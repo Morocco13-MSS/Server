4496bec8f42d44f0e7dce8fe89c156fd7b3cb66b
Jinzi Huang <jinzi.huang@gmail.com>
2018-10-22 20:10:40 +0100

    week-one-demo

--- api/routes/patientRoutes.js
+++ api/routes/patientRoutes.js
@@ -3,15 +3,58 @@
 module.exports = function(app, dbConnection) {
   var patient = require('../controllers/patientController');
 
-  var dbConnection = dbConnection;
   // Patient Routes
   // TODO: 
   // app.route('/Patients')
   //   .get(patient.getPatientAge);
 
+  // Globle view
+
+  app.route('/global').get(function (req, res){
+     //****************************ROW-2, Global View *********************************
+     var totalPatients = 0;
+     var curativeCount = 0;
+     var palliCount = 0;
+     var nACount = 0;
+     var missCount = 0;
+     
+     var queryString = 'SELECT item.intitule ,formulaire_item.valeur_item AS itemValue FROM `item`,`formulaire_item` WHERE item.intitule = "Résection"'
+     dbConnection.query(queryString,function (error,result, fields) 
+     {
+         if (error) throw error;
+         for (var i in result) 
+         {
+           totalPatients++;
+           if(result[i].itemValue == 0) //Not Applicatble 
+           {
+             nACount++;
+           }
+           else if(result[i].itemValue == 1) //A visée curative
+           {
+             curativeCount++;
+           }
+           else if(result[i].itemValue == 2) //Palliative
+           {
+             palliCount++;
+           }
+           else
+           {
+             missCount++;
+           }            
+         }
+         //Push the obtained data into Jason objectS
+         var globalViewObj  = {
+                         totalPatients:totalPatients,
+                         curativeCount:curativeCount,
+                         palliCount:palliCount,
+                         nACount:nACount,
+                         missCount,missCount};
+         res.json(globalViewObj);
+  });
+});
+
 // The following is a very simple route and api call, without calling controller
-  app.route('/Patients')
-    .get(function (req, res) {
+  app.route('/Patients').get(function (req, res) {
 
       var totalPatients = 0;
       var missingCount = 0;
@@ -40,8 +83,9 @@ module.exports = function(app, dbConnection) {
           lt70: totalPatients-gte70-missingCount,
           missing: missingCount
         };
+
         res.json(resultObj);
         }
       );
-    })
-};
+    });
+  }
--- server.js
+++ server.js
@@ -1,36 +1,72 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080,
+// var express = require('express'),
+//   app = express(),
+//   port = process.env.PORT || 8080,
 
-  bodyParser = require('body-parser');
-var cors = require('cors');
-var mysql = require('mysql');
-
-
-// Connect to the database
-console.log('Connecting to the database.....');
-var connection = mysql.createConnection({
- database: 'mssDB',  // TODO: change your database name
- host: "localhost",
- user: "root",
- password: "abcd1234!"
-});
-
-connection.connect(function(err) {
- if (err) throw err;
- console.log("Connected to the database!!");
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+//   bodyParser = require('body-parser');
+// var cors = require('cors');
+// var mysql = require('mysql');
 
 
-var routes = require('./api/routes/patientRoutes'); //importing route
-routes(app, connection); //register the route
+// // Connect to the database
+// console.log('Connecting to the database.....');
+// var connection = mysql.createConnection({
+//  database: 'mssDB',  // TODO: change your database name
+//  host: "localhost",
+//  user: "root",
+//  password: "abcd1234!"
+// });
+
+// connection.connect(function(err) {
+//  if (err) throw err;
+//  console.log("Connected to the database!!");
+// });
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cors());
 
 
-app.listen(port);
+// var routes = require('./api/routes/patientRoutes'); //importing route
+// routes(app, connection); //register the route
 
 
-console.log('RESTful API server started on: ' + port);
+// app.listen(port);
+
+
+// console.log('RESTful API server started on: ' + port);
+
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.get('http://localhost:3000/p/:id', (req, res) => {
+    const actualPage = '/post'
+    const queryParams = { title: req.params.id } 
+    app.render(req, res, actualPage, queryParams);
+    // axios.get('http://localhost:8080/patients') // REST get api call
+    // .then(response => {
+    //     console.log(response.data.gte70); 
+    //     app.render(req, res, actualPage, queryParams);
+    // });
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:8080')
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
