d331eb9125756c5e7bc6186f3bef71aa0ce2fc81
nmuruli <nmuruli@gmail.com>
2018-10-23 15:49:15 +0100

    Support the request object

--- api/controllers/globalController.js
+++ api/controllers/globalController.js
@@ -6,15 +6,21 @@ var Global = {
 
     //Global View ROW 2
     global: function (req, res) {
-        /********************************************************* */  
-        //TODO: Remove the below json object and get it as input
-        var startDate = "2018-01-01";
-        var endDate = "2019-01-01";
-        
-        var reqObj = {startDate:startDate,
-                    endDate:endDate,
-                    formType:'E',
-                    UnitType:1};  
+       /********************************************************* */  
+        console.log( req.query.startDate); 
+        console.log( req.query.endDate);
+        console.log( req.query.formType);
+        console.log( req.query.userLevel);
+        console.log( req.query.userId);    
+
+
+        // var startDate = "2018-01-01";
+        // var endDate = "2019-01-01";        
+        // var reqObj = {startDate:startDate,
+        //             endDate:endDate,
+        //             formType:'E',
+        //             UnitType:2,
+        //             userId:38};  
         /********************************************************* */
         var totalPatients = 0;
         var curativeCount = 0;
@@ -22,8 +28,9 @@ var Global = {
         var nACount = 0;
         var missCount = 0;
         
-        var queryString = 'SELECT formulaire.id, formulaire.id_organe, organe.id, organe.code, item.intitule , formulaire_item.id_formulaire , formulaire_item.valeur_item as itemValue FROM `formulaire`,`organe`,`item`,`formulaire_item` WHERE (formulaire.id_organe = organe.id AND organe.code = "E" AND formulaire.id = formulaire_item.id_formulaire) AND item.intitule = "Résection"';
-        db.query(queryString,function (error,result, fields) 
+        // var AllUnitQuery = 'SELECT patient.id,formulaire_item.valeur_item as itemValue,formulaire.id, formulaire.id_patient, formulaire.id_organe,formulaire_item.id_formulaire,formulaire_item.id,formulaire_item.valeur_item,item.intitule,organe.id, organe.code from patient , formulaire, formulaire_item, item, organe where ((patient.id = formulaire.id_patient) AND (formulaire.id_organe = organe.id AND organe.code = "'+ reqObj.formType +'" ) AND ((formulaire.id = formulaire_item.id_formulaire) AND (formulaire.date_creation BETWEEN "'+ reqObj.startDate + '" AND "'+ reqObj.endDate +'") AND (item.intitule = "Résection" AND formulaire_item.id_item = item.id)))';    
+        var AllUnitQuery = 'SELECT patient.id,formulaire_item.valeur_item as itemValue,formulaire.id, formulaire.id_patient, formulaire.id_organe,formulaire_item.id_formulaire,formulaire_item.id,formulaire_item.valeur_item,item.intitule,organe.id, organe.code from patient , formulaire, formulaire_item, item, organe where ((patient.id = formulaire.id_patient) AND (formulaire.id_organe = organe.id AND organe.code = "'+ req.query.formType +'" ) AND ((formulaire.id = formulaire_item.id_formulaire) AND (formulaire.date_creation BETWEEN "'+ req.query.startDate + '" AND "'+ req.query.endDate +'") AND (item.intitule = "Résection" AND formulaire_item.id_item = item.id)))';       
+        db.query(AllUnitQuery,function (error,result, fields) 
         {
             if (error) throw error;
             for (var i in result) 
