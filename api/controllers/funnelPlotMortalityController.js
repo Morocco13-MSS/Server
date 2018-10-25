034110407bf4e4b476a459cde2f1a7e39fbd9498
John Lin <linjo@us.ibm.com>
2018-10-24 20:41:07 -0400

    establishing nodeJS to R to mySQL interoperability

--- api/controllers/funnelPlotMortalityController.js
+++ api/controllers/funnelPlotMortalityController.js
@@ -5,14 +5,22 @@
 var db = require('../models/dbconnection'); 
 var R = require("r-script");
 
+
 //create class
 var Fpm = {
 
     //funnel plot derivation
     fpm: function (req, res) {
+        //TODO: only required for unit testing
+        req.query.startDate = "2018-01-01";
+        req.query.endDate = "2019-01-01";
+        req.query.formType = "E";
+        req.query.userLevel = 3;
+        req.query.userId = 2;
+//TODO: Rememove above lines
         //TODO: change path to where your path is
        var out = R("/Users/linjo/Documents/workspace-neon/mss-r-code/R-work/funnel-plot-mortality.R")
-        .data()
+        .data(req.query.startDate,req.query.endDate)
         .call(function(err, a) {
             if (err) throw err;
             res.json(a)
